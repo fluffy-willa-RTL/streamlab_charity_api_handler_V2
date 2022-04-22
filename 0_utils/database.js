b7fe613d494d2e96dd862bf5336bb9469c716087
dev@dreemcloud.net <fluffy.streamlab.api@gmail.com>
2022-04-22 12:25:44 +0200

    socket connection + database insert

---
+++ 0_utils/database.js
@@ -0,0 +1,5 @@
+let don = {}
+
+export default {
+	don
+}
--- 1_socket_streamlab/socket.js
+++ 1_socket_streamlab/socket.js
@@ -1,24 +1,23 @@
-
 import io 			from 'socket.io-client'
-import { Server } 	from 'socket.io'
+import Server 	from 'socket.io'
 
 import dotenv from 'dotenv'
 dotenv.config()
 
+import db			from '../0_utils/database.js'
+
 let streamlabs
 let front
-
 
 /**
  * Start the socket for both streamlab and front-end connection
  */
 function startSocket(server){
-	const url = process.env.STREAMLAB_SOCKET_URL + process.env.STREAMLAB_SOCKET
+	const streamlabUrl = 'https://sockets.streamlabs.com?token=' + process.env.SOCKET_WILLA
 
-	streamlabs = io(url, {transports: ['websocket']})
+	streamlabs = io(streamlabUrl, {transports: ['websocket']})
 	front = new Server(server, {cors: { origin: "*"}})
-
-
+	
 	streamlabs.on('connect', 	() => {console.log('connection to streamlabs successful')})
 	streamlabs.on('disconnect', () => {console.log('goodbye')})
 
@@ -27,12 +26,20 @@ function startSocket(server){
 
 	streamlabs.on('event', (data) => {
 		if (data.type === 'streamlabscharitydonation'){
-			console.log(data)
-			// console.log(data.message[0])
+			let _id	= data?.message?.[0]?.charityDonationId								?? parseInt(Math.random() * (10 ** 16)) //TODO REMOVE TESTING
+			let res = {
+				name		: data?.message?.[0]?.from									?? null,
+				message		: data?.message?.[0]?.message								?? null,
+				amount		: parseInt(parseFloat(data?.message?.[0]?.amount) * 100)	?? 0,
+				date		: Date.parse(data?.message?.[0]?.createdAt)	/ 1000			?? 0,
+				streamer_id	: data?.message?.[0]?.memberId								?? parseInt(Math.random() * (10 ** 16)), //TODO REMOVE TESTING
+			}
+			if (db.don[_id] === undefined){
+				db.don[_id] = res
+			}
 		}
 	})
 }
-
 
 export default {
 	startSocket,
