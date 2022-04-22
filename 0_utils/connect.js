73509148a9433b6491c91fc21615f6b21a4948a9
dev@dreemcloud.net <fluffy.streamlab.api@gmail.com>
2022-04-22 18:47:22 +0200

    add comment and create the `slug` key for make_request return

--- 0_utils/connect.js
+++ 0_utils/connect.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import 'dotenv/config' //for reading env
 
+import { slugify } from './slugify.js';
+
 export function get_auth_url() {
 	const paramsAutorization = {
 		'client_id'     : process.env.CLIENT_ID,
@@ -37,11 +39,14 @@ export async function refreshUserData(refreshToken) {
 
 async function	make_requests(postData)
 {
+	// Request the `access_token` to the api
 	const tokens = await axios.post(process.env.STREAMLAB_URL + 'token', postData)
 		.then((res) => {return res.data;})
 		.catch(() => {return null});
 	if (tokens === null)
 		return (null);
+
+	// Request user data from the api
 	const userdata = await axios.get(process.env.STREAMLAB_URL + 'user', {params: {
 		access_token: tokens.access_token
 	}}).then((res) => {return res.data;})
@@ -49,9 +54,12 @@ async function	make_requests(postData)
 	if (userdata === null)
 		return (null);
 	console.log(userdata);
+
+	//We add our custom `slug` for the frontend
 	return ({
 		id            : userdata.streamlabs.id,
 		display_name  : userdata.streamlabs.display_name,
+		slug          : slugify(userdata.streamlabs.display_name),
 		access_token  : tokens.access_token,
 		refresh_token : tokens.refresh_token
 	});
