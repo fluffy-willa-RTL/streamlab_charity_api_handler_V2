f0f54caa6031e8fd8b199acf5a9002fff141fc3e
dev@dreemcloud.net <fluffy.streamlab.api@gmail.com>
2022-04-23 13:50:25 +0200

    Add refresh client emit and comment function logic

--- 1_socket_streamlab/socket.js
+++ 1_socket_streamlab/socket.js
@@ -14,15 +14,19 @@ let front
  * Start the socket for both streamlab and front-end connection
  */
 function startSocket(server){
-	const streamlabUrl = 'https://sockets.streamlabs.com?token=' + process.env.SOCKET_WILLA
-
+	const streamlabUrl = 'https://sockets.streamlabs.com?token=' + process.env.SOCKET_DEV
+	// WS client /!\ streamlab use V2 server /!\
 	streamlabs = io(streamlabUrl, {transports: ['websocket']})
-	front = new Server(server, {cors: { origin: "*"}})
-	
-	streamlabs.on('connect', 		() 		=> {console.log('connection to streamlabs successful')})
-	streamlabs.on('connect_error', 	(err) 	=> {console.log('error:', err)})
-	streamlabs.on('disconnect', 	() 		=> {console.log('goodbye')})
 
+	// WS server
+	front = new Server(server, {cors: { origin: "*"}})
+
+	// Check te connection with the streamlab WS
+	streamlabs.on('connect', 		() 		=> {console.log('WS.client.[connect]')})
+	streamlabs.on('connect_error', 	(err) 	=> {console.log('WS.client.[connect_error]:', err)})
+	streamlabs.on('disconnect', 	() 		=> {console.log('WS.client.[disconnect]')})
+
+	// Listen all 'event' from streamlab WS
 	streamlabs.on('event', 	 (data) => {
 		if (data.type === 'streamlabscharitydonation'){
 			let _id	= data?.message?.[0]?.charityDonationId								?? parseInt(Math.random() * (10 ** 16)) //TODO REMOVE TESTING
@@ -33,20 +37,24 @@ function startSocket(server){
 				date		: Date.parse(data?.message?.[0]?.createdAt)	/ 1000			?? 0,
 				streamer_id	: data?.message?.[0]?.memberId								?? 72567 //parseInt(Math.random() * (10 ** 16)), //TODO REMOVE TESTING
 			}
+
+			// Check if the id exist in the db
 			if (db.don[_id] === undefined){
 				db.don[_id] = res
 			}
-			
+			//TODO why ? @willa
 			updateFront(streamlabs)
 		}
 	})
 	
-	
+	// Listen all incoming connection of the 
 	front.on('connect', (data) => {
-		console.log('connection to the front successful', data.id)
-
+		console.log('WS.server.[connect]', data.id)
+		// Client will ask `whoami` to recive all streamer info (slug, name, id, PP)
 		data.on('whoami',(res) => {
 			if (res['slug'] !== undefined){
+				console.log(`WS.server.[whoami] from ${res.slug}`);//DEBUG show when client ask whoami
+				// Try to find the streamer in the team
 				for (var streamer of Object.values(db.streamer)){
 					if (streamer.slug === res.slug){
 						data.emit('youare', streamer);
@@ -54,15 +62,23 @@ function startSocket(server){
 					}
 				}
 			}
-			// If the user is not found return to the front `-404`
+			// If the user is not found return to the front "error" `-404`
 			data.emit('youare', {"error": 404});
 		})
-
+		//TODO @willa why ?
 		data.on('refresh-streamer',(res) => {
 			db.getAllStreamer()
 		})
 	})
 }
+
+/**
+ * /!\ Will force refresh of all client /!\
+ */
+	function forceRefreshClient() {
+	front.emit('forceRefresh', null);
+}
+
 
 export default {
 	startSocket, forceRefreshClient
