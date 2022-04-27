38a98a472203e0d90de5b84f3eb44afa02d49d15
dev@dreemcloud.net <fluffy.streamlab.api@gmail.com>
2022-04-27 12:06:17 +0200

    Major rework of structure of the web part
    Co-authored-by: Fluffy_Kaiju <Matthew-Dreemurr@users.noreply.github.com>

---
+++ server/4_web/public/script/animateText.js
@@ -0,0 +1,24 @@
+const moneyConverter = Intl.NumberFormat('fr-FR', {
+	style: 'currency',
+	currency: 'EUR',
+});
+
+/*
+input :
+	obj: {
+		diplay: xxx		=> set to set 0 as global variable
+		amount: xxx		=> set to set 0 as global variable
+	}
+	id:
+		css id of the item we want to change
+*/
+function animateMoney(obj, id) {
+	const interval = setInterval(function() {
+		if (obj.display === obj.amount)
+			clearInterval(interval);
+		let change = (obj.amount - obj.display) / 10;
+		change = change >= 0 ? Math.ceil(change) : Math.floor(change);
+		obj.display += change;
+		document.getElementById(id).textContent = moneyConverter.format(obj.display / 100);
+	}, 20);
+};
---
+++ server/4_web/public/src/asset/streamer/donation/donationLast.js
@@ -0,0 +1,28 @@
+let totalStreamer = {
+	amount: 0,
+	display: 0
+}
+
+async function start() {
+	const	id = window.location.pathname.split('/')[2]
+
+	if (id)
+	{
+		document.getElementById('streamerId').textContent = id;
+		
+		const socket = await connect();
+		// Listen update for the total amount
+		socket.emit('init');
+
+		socket.on(`donation_last.${id}`, (res) => {
+			console.log(res)
+			// totalStreamer.amount = res;
+			// animateMoney(totalStreamer, 'totalStreamer');
+		});
+
+	} else {
+		document.location = '/u/'
+	}
+}
+
+start();
