85a518bfd7a0bf0ac9194d7d76801b4ea31f8bca
dev@dreemcloud.net <fluffy.streamlab.api@gmail.com>
2022-04-27 16:03:08 +0200

    front wip

--- server/4_web/public/src/asset/streamer/donation/donationLast.js
+++ server/4_web/public/src/asset/streamer/donation/donationLast.js
@@ -1,8 +1,3 @@
-const totalStreamer = {
-	amount: 0,
-	display: 0
-}
-
 async function start() {
 	const	id = window.location.pathname.split('/')[2]
 
@@ -15,11 +10,22 @@ async function start() {
 		socket.emit('init');
 
 		socket.on(`donation_last.${id}`, (res) => {
-			console.log(res)
-			// totalStreamer.amount = res;
-			// animateMoney(totalStreamer, 'totalStreamer');
-		});
+			for (let i in res){
+				if (document.getElementById(`donation_${i}`) == null){
+					const newElem = (document.getElementById("templateDonation")).content.cloneNode(true);
+					newElem.getElementById(`donation`).id = `donation_${i}`
+					document.body.appendChild(newElem)
+				}
+				let elem = document.getElementById(`donation_${i}`)
+				elem.getElementsByClassName('name')[0].textContent = res[i].name
+				elem.getElementsByClassName('value')[0].textContent = res[i].amount
+				elem.getElementsByClassName('message')[0].textContent = res[i].message
 
+				console.log(elem)
+				// document.body.appendChild(elem)
+				// console.log(elem.getElementsByClassName('name').textContent)
+			}
+		});
 	} else {
 		document.location = '/u/'
 	}
--- server/4_web/public/src/u.js
+++ server/4_web/public/src/u.js
@@ -1,49 +1,79 @@
-async function start() {
-	// window.location = 'https://google.com'
+let linkToGenerate = null;
+
+async function start () {
 	if (slug) {
 		// Try to connect to the backen socket
 		const socket = await io(`ws://${window.location.hostname}`, {
-			reconnectionDelayMax: 5000,
+			reconnectionDelayMax: 5000,//TODO check the doc
 		});
-		
+
 		// Listen if a connection error occur
-		socket.on('connect_error', () => {console.log('err');})
+		socket.on('connect_error', (err) => {console.log(`err`, err);})
 
 		// Listen if we lost connection
 		socket.on('disconnect', () => {
-			console.log('streamlabs disconnect')
-			document.getElementById('welcomeMessage').textContent = 'Connexion perdue, reconnexion en cours.\nSi cela prend plus d’une minute, contactez les dev.';
-		})
+			console.log('WS disconnect')
+		});
 
-		// Listen if a connection have made with the backend
+		// Forece refresh the tab
+		socket.on('forceRefresh', () => {
+			document.location.reload(true)
+		});
+
 		socket.on('connect', () => {
-			console.log('Connected!')
-			
 			// Ask the streamer data
 			socket.emit('whoami', {"slug":slug})
 			// Listen for the streamer data
 			socket.on('youare', (res) => {
 				// If the slug is in the `res` init data and return the function
+				// If the back dont find the user
+				if (res && 'error' in res) {
+					document.getElementById('welcomeMessage').textContent = `On ne t'a pas trouvé dans la team, vérifie que tu as bien lié ton compte Streamlabs avec la campagne.\nRends-toi sur http://dev.fluffy.dreemcloud.net/u/ pour plus d'info.`;
+					window.location = '/u/';
+				}
 				if (res && 'streamer' in res) {
 					data = res;
 					document.getElementById('welcomeMessage').textContent =  `Salut ${data.streamer.display_name}`;
 					console.log(`Salut ${data.streamer.display_name}, tu n'es pas censé être là, ouste !`);
 					console.log(data);
-					return ;
+					// Show all component
+					if (linkToGenerate === null) {
+						linkToGenerate = [
+							{
+								title: `Total récolté par ${data.streamer.display_name}`,
+								src: `/a/${data.id}/total/me`,
+							},
+							{
+								title: `Total récolté par la Team`,
+								src: `/a/${data.id}/total/all`,
+							},
+							{
+								title: `Total récolté par ${data.streamer.display_name} et la Team`,
+								src: `/a/${data.id}/total/me-all`,
+							},
+						];
+						const list = document.getElementById("myList");
+						linkToGenerate.forEach((item) => {
+							const div = document.createElement('div');
+							div.style = '  margin: auto;   width: 50%;   border: 3px solid green;   padding: 10px;'
+							const title = document.createElement("a");
+							title.textContent = item.title;
+							title.href = item.src;
+							div.appendChild(title);
+							const frame = document.createElement("iframe");
+							frame.src = item.src;
+							frame.width = 200;
+							frame.height = 250;
+							div.appendChild(frame);
+							list.appendChild(div);
+						});
+					}
 				}
-				
-				// If the back dont find the user
-				if (res && 'error' in res) {
-					document.getElementById('welcomeMessage').textContent = `On ne t'a pas trouvé dans la team, vérifie que tu as bien lié ton compte Streamlabs avec la campagne.\nRends-toi sur http://dev.fluffy.dreemcloud.net/u/ pour plus d'info.`;
-				}
-				window.location = '/u/';
 			})
 		})
-
-		socket.on('forceRefresh', () => {
-			document.location.reload(true)
-		})
+		return ;
 	}
+	window.location = '/u/';
 }
 
 start();
