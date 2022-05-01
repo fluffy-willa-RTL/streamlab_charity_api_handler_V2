08075cbf2af26d2023f4089918f336abfa9ae5b2
matthew_dreemurr <hadadmat@gmail.com>
2022-05-01 20:42:38 +0000

    log utils

---
+++ server/0_utils/log.js
@@ -0,0 +1,17 @@
+import { Console } from 'node:console';
+import fs from 'node:fs';
+
+export function log(msg) {
+	console.log(`[${new Date().toLocaleTimeString()}]: ${msg}`);
+	logFile.log(`[${new Date().toLocaleTimeString()}]: ${msg}`);
+}
+
+export function logErr(msg) {
+	console.error();(`[${new Date().toLocaleTimeString()}]: ${msg}`);
+	logFile.error();(`[${new Date().toLocaleTimeString()}]: ${msg}`);
+}
+
+export const logFile = new Console({
+	stdout: fs.createWriteStream("log.txt"),
+	stderr: fs.createWriteStream("errLog.txt"),
+});
