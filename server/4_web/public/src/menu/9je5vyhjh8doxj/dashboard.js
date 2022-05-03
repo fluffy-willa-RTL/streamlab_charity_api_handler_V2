b057bdc0d00cf81071ea28f7138f46c7e2dc7181
dev@dreemcloud.net <fluffy.streamlab.api@gmail.com>
2022-05-03 17:58:43 +0200

    Fix css
    Co-authored-by: lorenuar.s19 <lorenuar.s19@users.noreply.github.com>

--- server/4_web/public/src/menu/9je5vyhjh8doxj/dashboard.js
+++ server/4_web/public/src/menu/9je5vyhjh8doxj/dashboard.js
@@ -7,14 +7,7 @@ async function start() {
 		for (const item of Object.values(data)) {
 			const div = document.createElement(`div`);
 			div.className = `listElement`;
-
-			const link = document.createElement("a");
-			link.textContent		= item?.display_name;
-			link.href				= `/u/${item?.slug}`;
-			link.className			= 'nameLink';
-			link.target			= '_blank';
-			div.appendChild(link);
-
+			
 			const linkImg = document.createElement("a");
 			linkImg.href = `/u/${item?.slug}`;
 			const img = document.createElement("img");
@@ -22,6 +15,13 @@ async function start() {
 			img.href = `/u/${item?.slug}`;
 			linkImg.appendChild(img);
 			div.appendChild(linkImg);
+			
+						const link = document.createElement("a");
+						link.textContent		= item?.display_name;
+						link.href				= `/u/${item?.slug}`;
+						link.className			= 'nameLink';
+						link.target			= '_blank';
+						div.appendChild(link);
 
 			list.appendChild(div);
 		}
