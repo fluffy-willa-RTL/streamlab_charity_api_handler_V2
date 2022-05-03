53488e1726d55da86fa99a6ac2e7fde1166584fa
dev@dreemcloud.net <fluffy.streamlab.api@gmail.com>
2022-05-03 22:24:02 +0200

    better log

--- server/0_utils/log.js
+++ server/0_utils/log.js
@@ -1,26 +1,44 @@
 import { Console } from 'node:console';
 import fs from 'node:fs';
 
-const startTime = new Date().toLocaleTimeString();
+// Set the __diname 
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+var startTime = date24Now();
+
+// const startTime = date.toLocaleTimeString("fr-BE", {hour12: false});
+function date24Now () {
+	return new Date().toLocaleTimeString("fr-BE", {hour12: false});
+}
 
 export function log(msg) {
-	console.log(`[${new Date().toLocaleTimeString()}]: ${msg}`);
-	logFile.log(`[${new Date().toLocaleTimeString()}]: ${msg}`);
+	// const dateNow = date.toLocaleTimeString();
+	const dateNow = date24Now();
+	console.log(`[${dateNow}]: ${msg}`);
+	logFile.log(`[${dateNow}]: ${msg}`);
 }
 
 export function logTable(msg, option) {
-	console.log(`[${new Date().toLocaleTimeString()}]`);
+	// const dateNow = date.toLocaleTimeString("fr-BE", {hour12: false});
+	const dateNow = date24Now();
+	console.log(`[${dateNow}]`);
 	console.table(msg, option);
-	logFile.log(`[${new Date().toLocaleTimeString()}]`);
+	logFile.log(`[${dateNow}]`);
 	logFile.table(msg, option);
 }
 
 export function logErr(msg) {
-	console.log(`[${new Date().toLocaleTimeString()}]: ${msg}`);
-	logFile.error(`[${new Date().toLocaleTimeString()}]: ${msg}`);
+	// const dateNow = date.toLocaleTimeString("fr-BE", {hour12: false});
+	const dateNow = date24Now();
+	console.log(`[${dateNow}]: ${msg}`);
+	logFile.error(`[${dateNow}]: ${msg}`);
 }
 
+const logFilePath = join(__dirname, "..", "log", `log_${startTime}.log`);
+const errFilePath = join(__dirname, "..", "log", `err_${startTime}.log`);
 export const logFile = new Console({
-	stdout: fs.createWriteStream(`log_${startTime}.txt`),
-	stderr: fs.createWriteStream(`errLog_${startTime}.txt`),
+	stdout: fs.createWriteStream(logFilePath),
+	stderr: fs.createWriteStream(errFilePath),
 });
